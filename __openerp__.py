291ac8e1771092eccf21062b9456cb479ef4bbca
Cristian Sebastian Rocha <crocha@kekkaishi.jjdiazarana.net>
2012-02-01 22:27:47 -0300

    v2.0

--- __openerp__.py
+++ __openerp__.py
@@ -20,7 +20,7 @@
 
 {
     'name': 'Partner Title in Argentina',
-    'version': '1.0',
+    'version': '2.0',
     'author': 'Thymbra - Torre de Hanoi',
     'category': 'Localisation/Argentina',
     'website': 'http://www.thymbra.com/',
